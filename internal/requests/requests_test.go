97c5b9e8a7b4ab3c31c2591a624d0d109f695c3a
jacobmeredith <jacobmeredith@hotmail.co.uk>
2024-01-02 21:29:38 +0000

    fix broken tests and remove collections directory

--- internal/requests/requests_test.go
+++ internal/requests/requests_test.go
@@ -6,16 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestParseKeyValuePairString(t *testing.T) {
+func TestSplitKeyValuePairString(t *testing.T) {
 	headerString := "authorization:Bearer token,x-test-header:test"
-	parsed := ParseKeyValuePairString(headerString, ",", ":")
+	parsed := splitKeyValuePairString(headerString, ",", ":")
 
 	assert.Len(t, parsed, 2, "Length should be 2")
 	assert.Equal(t, "Bearer token", parsed["authorization"])
 	assert.Equal(t, "test", parsed["x-test-header"])
 
 	headerString = ""
-	parsed = ParseKeyValuePairString(headerString, ",", ":")
+	parsed = splitKeyValuePairString(headerString, ",", ":")
 
 	assert.Len(t, parsed, 0)
 }
